06becd5e38b732bf7d1f528c855270460addcd1e
SIEUN <shyumee@gmail.com>
2020-10-14 18:16:15 +0900

    .gitignore 파일 추가

--- src/main/java/com/todolist/se/web/IndexController.java
+++ src/main/java/com/todolist/se/web/IndexController.java
@@ -2,6 +2,7 @@ package com.todolist.se.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
@@ -10,4 +11,5 @@ public class IndexController {
   public String index(){
     return "hello :D";
   }
+
 }
