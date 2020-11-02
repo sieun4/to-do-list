095be7e7fda2193dd369794b6f32833d1dcc19bc
SIEUN <shyumee@gmail.com>
2020-11-02 12:47:51 +0900

    등록

--- src/main/java/com/todolist/se/service/posts/PostsService.java
+++ src/main/java/com/todolist/se/service/posts/PostsService.java
@@ -1,5 +1,20 @@
 package com.todolist.se.service.posts;
 
+import com.todolist.se.domain.posts.PostsRepository;
+import com.todolist.se.web.dto.PostsSaveRequestDto;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+
+  private final PostsRepository postsRepository;
+
+  @Transactional
+  public Long save(PostsSaveRequestDto requestDto){
+    return postsRepository.save(requestDto.toEntity()).getId();
+  }
 
 }
